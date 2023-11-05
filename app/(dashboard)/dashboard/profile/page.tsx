e983500ea3622ea37ce2539c503eea5b7d1a6447
Kiranism <iamkiran2831@gmail.com>
2023-11-06 00:44:30 +0530

    multistep form

---
+++ app/(dashboard)/dashboard/profile/page.tsx
@@ -0,0 +1,16 @@
+import BreadCrumb from "@/components/breadcrumb";
+import { CreateProfileOne } from "@/components/forms/user-profile-stepper/create-profile";
+import { UserClient } from "@/components/tables/user-tables/client";
+import { users } from "@/constants/data";
+
+const breadcrumbItems = [{ title: "Profile", link: "/dashboard/profile" }];
+export default function page() {
+  return (
+    <>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BreadCrumb items={breadcrumbItems} />
+        <CreateProfileOne categories={[]} initialData={null} />
+      </div>
+    </>
+  );
+}
---
+++ hooks/use-multistepform.tsx
@@ -0,0 +1,34 @@
+import { ReactElement, useState } from "react"
+
+export function useMultistepForm(steps: ReactElement[]) {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+
+  function next() {
+    setCurrentStepIndex(i => {
+      if (i >= steps.length - 1) return i
+      return i + 1
+    })
+  }
+
+  function back() {
+    setCurrentStepIndex(i => {
+      if (i <= 0) return i
+      return i - 1
+    })
+  }
+
+  function goTo(index: number) {
+    setCurrentStepIndex(index)
+  }
+
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    steps,
+    isFirstStep: currentStepIndex === 0,
+    isLastStep: currentStepIndex === steps.length - 1,
+    goTo,
+    next,
+    back,
+  }
+}
