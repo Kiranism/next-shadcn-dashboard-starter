42a211fe9423ad5d1dea8d11f230d1d3330a3f3e
Kiranism <iamkiran2831@gmail.com>
2023-11-07 18:03:00 +0530

    update

--- app/(dashboard)/dashboard/profile/page.tsx
+++ app/(dashboard)/dashboard/profile/page.tsx
@@ -7,7 +7,7 @@ const breadcrumbItems = [{ title: "Profile", link: "/dashboard/profile" }];
 export default function page() {
   return (
     <>
-      <div className="flex-1 space-y-4 p-8 pt-6">
+      <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
         <BreadCrumb items={breadcrumbItems} />
         <CreateProfileOne categories={[]} initialData={null} />
       </div>
