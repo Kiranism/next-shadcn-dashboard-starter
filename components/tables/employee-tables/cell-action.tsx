1f164a2932597702531b336c5ee056991e2c201a
Kiranism <iamkiran2831@gmail.com>
2024-02-13 13:04:51 +0530

    fix:Re-enable pointer events in modal

--- components/tables/employee-tables/cell-action.tsx
+++ components/tables/employee-tables/cell-action.tsx
@@ -32,7 +32,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
         onConfirm={onConfirm}
         loading={loading}
       />
-      <DropdownMenu>
+      <DropdownMenu modal={false}>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="h-8 w-8 p-0">
             <span className="sr-only">Open menu</span>
