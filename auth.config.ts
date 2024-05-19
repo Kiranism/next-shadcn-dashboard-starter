64a43166d15da6a6d1950e939489736016525b28
Kiranism <iamkiran2831@gmail.com>
2024-05-19 22:53:08 +0530

    code formatted

--- auth.config.ts
+++ auth.config.ts
@@ -1,24 +1,28 @@
-import { NextAuthConfig } from "next-auth";
-import CredentialProvider from "next-auth/providers/credentials";
-import GithubProvider from "next-auth/providers/github";
+import { NextAuthConfig } from 'next-auth';
+import CredentialProvider from 'next-auth/providers/credentials';
+import GithubProvider from 'next-auth/providers/github';
 
 const authConfig = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? '',
-      clientSecret: process.env.GITHUB_SECRET ?? '',
+      clientSecret: process.env.GITHUB_SECRET ?? ''
     }),
     CredentialProvider({
       credentials: {
         email: {
-          type: 'email',
+          type: 'email'
         },
         password: {
-          type: 'password',
+          type: 'password'
         }
       },
       async authorize(credentials, req) {
-        const user = { id: '1', name: 'John', email: credentials?.email as string };
+        const user = {
+          id: '1',
+          name: 'John',
+          email: credentials?.email as string
+        };
         if (user) {
           // Any object returned will be saved in `user` property of the JWT
           return user;
@@ -32,8 +36,8 @@ const authConfig = {
     })
   ],
   pages: {
-    signIn: "/", //sigin page
-  },
-} satisfies NextAuthConfig
+    signIn: '/' //sigin page
+  }
+} satisfies NextAuthConfig;
 
-export default authConfig
+export default authConfig;
