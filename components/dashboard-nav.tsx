8302db7e45029344480a65105ce8fbe0b3462528
Kiranism <iamkiran2831@gmail.com>
2024-05-22 21:13:04 +0530

    feat: collapisible sidebar

--- components/dashboard-nav.tsx
+++ components/dashboard-nav.tsx
@@ -7,46 +7,76 @@ import { Icons } from '@/components/icons';
 import { cn } from '@/lib/utils';
 import { NavItem } from '@/types';
 import { Dispatch, SetStateAction } from 'react';
+import { useSidebar } from '@/hooks/useSidebar';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from './ui/tooltip';
 
 interface DashboardNavProps {
   items: NavItem[];
   setOpen?: Dispatch<SetStateAction<boolean>>;
+  isMobileNav?: boolean;
 }
 
-export function DashboardNav({ items, setOpen }: DashboardNavProps) {
+export function DashboardNav({
+  items,
+  setOpen,
+  isMobileNav = false
+}: DashboardNavProps) {
   const path = usePathname();
+  const { isMinimized } = useSidebar();
 
   if (!items?.length) {
     return null;
   }
 
+  console.log('isActive', isMobileNav, isMinimized);
+
   return (
     <nav className="grid items-start gap-2">
-      {items.map((item, index) => {
-        const Icon = Icons[item.icon || 'arrowRight'];
-        return (
-          item.href && (
-            <Link
-              key={index}
-              href={item.disabled ? '/' : item.href}
-              onClick={() => {
-                if (setOpen) setOpen(false);
-              }}
-            >
-              <span
-                className={cn(
-                  'group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground',
-                  path === item.href ? 'bg-accent' : 'transparent',
-                  item.disabled && 'cursor-not-allowed opacity-80'
-                )}
-              >
-                <Icon className="mr-2 h-4 w-4" />
-                <span>{item.title}</span>
-              </span>
-            </Link>
-          )
-        );
-      })}
+      <TooltipProvider>
+        {items.map((item, index) => {
+          const Icon = Icons[item.icon || 'arrowRight'];
+          return (
+            item.href && (
+              <Tooltip key={index}>
+                <TooltipTrigger asChild>
+                  <Link
+                    href={item.disabled ? '/' : item.href}
+                    className={cn(
+                      'flex items-center gap-2 overflow-hidden rounded-md py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground',
+                      path === item.href ? 'bg-accent' : 'transparent',
+                      item.disabled && 'cursor-not-allowed opacity-80'
+                    )}
+                    onClick={() => {
+                      if (setOpen) setOpen(false);
+                    }}
+                  >
+                    <Icon className={`ml-3 size-5`} />
+
+                    {isMobileNav || (!isMinimized && !isMobileNav) ? (
+                      <span className="mr-2 truncate">{item.title}</span>
+                    ) : (
+                      ''
+                    )}
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent
+                  align="center"
+                  side="right"
+                  sideOffset={8}
+                  className={!isMinimized ? 'hidden' : 'inline-block'}
+                >
+                  {item.title}
+                </TooltipContent>
+              </Tooltip>
+            )
+          );
+        })}
+      </TooltipProvider>
     </nav>
   );
 }
--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -1,18 +1,43 @@
+'use client';
+import React, { useState } from 'react';
 import { DashboardNav } from '@/components/dashboard-nav';
 import { navItems } from '@/constants/data';
 import { cn } from '@/lib/utils';
+import { ChevronLeft } from 'lucide-react';
+import { useSidebar } from '@/hooks/useSidebar';
 
-export default function Sidebar() {
+type SidebarProps = {
+  className?: string;
+};
+
+export default function Sidebar({ className }: SidebarProps) {
+  const { isMinimized, toggle } = useSidebar();
+  const [status, setStatus] = useState(false);
+
+  const handleToggle = () => {
+    setStatus(true);
+    toggle();
+    setTimeout(() => setStatus(false), 500);
+  };
   return (
     <nav
-      className={cn(`relative hidden h-screen w-72 border-r pt-16 lg:block`)}
+      className={cn(
+        `relative hidden h-screen border-r pt-20 md:block`,
+        status && 'duration-500',
+        !isMinimized ? 'w-72' : 'w-[72px]',
+        className
+      )}
     >
+      <ChevronLeft
+        className={cn(
+          'absolute -right-3 top-20 cursor-pointer rounded-full border bg-background text-3xl text-foreground',
+          !isMinimized && 'rotate-180'
+        )}
+        onClick={handleToggle}
+      />
       <div className="space-y-4 py-4">
         <div className="px-3 py-2">
-          <div className="space-y-1">
-            <h2 className="mb-2 px-4 text-xl font-semibold tracking-tight">
-              Overview
-            </h2>
+          <div className="mt-3 space-y-1">
             <DashboardNav items={navItems} />
           </div>
         </div>
