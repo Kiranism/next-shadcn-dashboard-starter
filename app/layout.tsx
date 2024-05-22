0c71be3f12e3ca517be4d28ac65b839e738063a9
Kiranism <iamkiran2831@gmail.com>
2024-05-22 21:18:24 +0530

    feat: Next Top loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Providers from '@/components/layout/providers';
 import { Toaster } from '@/components/ui/toaster';
 import '@uploadthing/react/styles.css';
 import type { Metadata } from 'next';
+import NextTopLoader from 'nextjs-toploader';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { auth } from '@/auth';
@@ -22,6 +23,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} overflow-hidden`}>
+        <NextTopLoader />
         <Providers session={session}>
           <Toaster />
           {children}
