ac5484be3be7324388e5a16d39a5af23a38b0f82
Arsen Babajanyan <arsenbabajanyan95@gmail.com>
2024-06-16 00:42:39 +0000

    FIX: sidebar toggle full clickable

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -22,7 +22,7 @@ export default function Sidebar({ className }: SidebarProps) {
   return (
     <nav
       className={cn(
-        `relative hidden h-screen flex-none border-r pt-20 md:block`,
+        `relative hidden h-screen flex-none border-r z-10 pt-20 md:block`,
         status && 'duration-500',
         !isMinimized ? 'w-72' : 'w-[72px]',
         className
