f45a966f8fb92abf5ce3c92237bf6e6105eef7b9
Kiranism <iamkiran2831@gmail.com>
2024-07-24 18:35:24 +0530

    feat: sidebar and header styling

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -2,35 +2,14 @@ import ThemeToggle from '@/components/layout/ThemeToggle/theme-toggle';
 import { cn } from '@/lib/utils';
 import { MobileSidebar } from './mobile-sidebar';
 import { UserNav } from './user-nav';
-import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur">
-      <nav className="flex h-14 items-center justify-between px-4">
-        <div className="hidden lg:block">
-          <Link
-            href={'https://github.com/Kiranism/next-shadcn-dashboard-starter'}
-            target="_blank"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-          </Link>
-        </div>
+    <div className="absolute left-0 right-0 top-0 bg-background/10 backdrop-blur">
+      <nav className="flex h-14 items-center justify-between px-4 md:justify-end">
         <div className={cn('block lg:!hidden')}>
           <MobileSidebar />
         </div>
-
         <div className="flex items-center gap-2">
           <UserNav />
           <ThemeToggle />
--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -5,6 +5,7 @@ import { navItems } from '@/constants/data';
 import { cn } from '@/lib/utils';
 import { ChevronLeft } from 'lucide-react';
 import { useSidebar } from '@/hooks/useSidebar';
+import Link from 'next/link';
 
 type SidebarProps = {
   className?: string;
@@ -22,15 +23,34 @@ export default function Sidebar({ className }: SidebarProps) {
   return (
     <nav
       className={cn(
-        `relative hidden h-screen flex-none border-r z-10 pt-20 md:block`,
+        `relative  hidden h-screen flex-none border-r bg-card md:block`,
         status && 'duration-500',
         !isMinimized ? 'w-72' : 'w-[72px]',
         className
       )}
     >
+      <div className="hidden p-5 pt-10 lg:block">
+        <Link
+          href={'https://github.com/Kiranism/next-shadcn-dashboard-starter'}
+          target="_blank"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="mr-2 h-6 w-6"
+          >
+            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+          </svg>
+        </Link>
+      </div>
       <ChevronLeft
         className={cn(
-          'absolute -right-3 top-20 cursor-pointer rounded-full border bg-background text-3xl text-foreground',
+          'absolute -right-3 top-10 z-50  cursor-pointer rounded-full border bg-background text-3xl text-foreground',
           isMinimized && 'rotate-180'
         )}
         onClick={handleToggle}
