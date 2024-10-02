58c0ef46e0be374f41f0e69dd87369955af03199
Kiranism <iamkiran2831@gmail.com>
2024-10-03 01:13:05 +0530

    fix: lint

--- sections/employee/views/employee-view-page.tsx
+++ sections/employee/views/employee-view-page.tsx
@@ -1,7 +1,5 @@
 import { Breadcrumbs } from '@/components/breadcrumbs';
-import { ProductForm } from '@/components/forms/product-form';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import React from 'react';
 import EmployeeForm from '../employee-form';
 
 const breadcrumbItems = [
