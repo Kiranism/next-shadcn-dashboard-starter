e71246f5722f20a8db2f4f84a552fb0878c06222
Kiranism <iamkiran2831@gmail.com>
2024-10-17 17:39:56 +0530

    fix: removed conflicted import css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import Providers from '@/components/layout/providers';
 import { Toaster } from '@/components/ui/toaster';
-import '@uploadthing/react/styles.css';
 import type { Metadata } from 'next';
 import NextTopLoader from 'nextjs-toploader';
 import { Inter } from 'next/font/google';
