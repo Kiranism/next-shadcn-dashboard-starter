b94eb78bf705bfed7d1bfe0db6e66d08aa6b6943
Kiranism <iamkiran2831@gmail.com>
2024-10-17 23:10:29 +0530

    feat: added suspense boundary

--- app/dashboard/product/page.tsx
+++ app/dashboard/product/page.tsx
@@ -1,18 +1,64 @@
-import { searchParamsCache } from '@/lib/searchparams';
+import { Breadcrumbs } from '@/components/breadcrumbs';
+import PageContainer from '@/components/layout/page-container';
+import { buttonVariants } from '@/components/ui/button';
+import { Heading } from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
+import { DataTableSkeleton } from '@/components/ui/table/data-table-skeleton';
+import { searchParamsCache, serialize } from '@/lib/searchparams';
+import { cn } from '@/lib/utils';
+import ProductTableAction from '@/sections/product/product-tables/product-table-action';
 import { ProductListingPage } from '@/sections/product/view';
+import { Plus } from 'lucide-react';
+import Link from 'next/link';
 import { SearchParams } from 'nuqs/parsers';
+import { Suspense } from 'react';
+
+const breadcrumbItems = [
+  { title: 'Dashboard', link: '/dashboard' },
+  { title: 'Products', link: '/dashboard/product' }
+];
+
+export const metadata = {
+  title: 'Dashboard: Products'
+};
 
 type pageProps = {
   searchParams: SearchParams;
-};
-
-export const metadata = {
-  title: 'Dashboard : Product'
 };
 
 export default async function Page({ searchParams }: pageProps) {
   // Allow nested RSCs to access the search params (in a type-safe way)
   searchParamsCache.parse(searchParams);
 
-  return <ProductListingPage />;
+  const key = serialize({ ...searchParams });
+
+  console.log('key', key);
+
+  return (
+    <PageContainer>
+      <div className="space-y-4">
+        <Breadcrumbs items={breadcrumbItems} />
+        <div className="flex items-start justify-between">
+          <Heading
+            title="Products"
+            description="Manage products (Server side table functionalities.)"
+          />
+          <Link
+            href="/dashboard/product/new"
+            className={cn(buttonVariants(), 'text-xs md:text-sm')}
+          >
+            <Plus className="mr-2 h-4 w-4" /> Add New
+          </Link>
+        </div>
+        <Separator />
+        <ProductTableAction />
+        <Suspense
+          key={key}
+          fallback={<DataTableSkeleton columnCount={5} rowCount={9} />}
+        >
+          <ProductListingPage />
+        </Suspense>
+      </div>
+    </PageContainer>
+  );
 }
--- sections/product/view/product-listing.tsx
+++ sections/product/view/product-listing.tsx
@@ -1,21 +1,7 @@
-import { Breadcrumbs } from '@/components/breadcrumbs';
-import PageContainer from '@/components/layout/page-container';
-import ProductTable from '../product-tables';
-import { buttonVariants } from '@/components/ui/button';
-import { Heading } from '@/components/ui/heading';
-import { Separator } from '@/components/ui/separator';
 import { Product } from '@/constants/data';
 import { fakeProducts } from '@/constants/mock-api';
 import { searchParamsCache } from '@/lib/searchparams';
-import { cn } from '@/lib/utils';
-import { Plus } from 'lucide-react';
-import Link from 'next/link';
-import type { SearchParams } from 'nuqs/server';
-
-const breadcrumbItems = [
-  { title: 'Dashboard', link: '/dashboard' },
-  { title: 'Products', link: '/dashboard/product' }
-];
+import ProductTable from '../product-tables';
 
 type ProductListingPage = {};
 
@@ -37,25 +23,5 @@ export default async function ProductListingPage({}: ProductListingPage) {
   const totalProducts = data.total_products;
   const products: Product[] = data.products;
 
-  return (
-    <PageContainer>
-      <div className="space-y-4">
-        <Breadcrumbs items={breadcrumbItems} />
-        <div className="flex items-start justify-between">
-          <Heading
-            title={`Products (${totalProducts})`}
-            description="Manage products (Server side table functionalities.)"
-          />
-          <Link
-            href={'/dashboard/product/new'}
-            className={cn(buttonVariants(), 'text-xs md:text-sm')}
-          >
-            <Plus className="mr-2 h-4 w-4" /> Add New
-          </Link>
-        </div>
-        <Separator />
-        <ProductTable data={products} totalData={totalProducts} />
-      </div>
-    </PageContainer>
-  );
+  return <ProductTable data={products} totalData={totalProducts} />;
 }
