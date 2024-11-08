412054598ad7cc943d7295c2f44bfec1ce69d5b4
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2024-11-08 21:44:23 +0530

    Update app-sidebar.tsx
    this will make table responsive in smaller devices

--- components/layout/app-sidebar.tsx
+++ components/layout/app-sidebar.tsx
@@ -237,7 +237,7 @@ export default function AppSidebar({
         </SidebarFooter>
         <SidebarRail />
       </Sidebar>
-      <SidebarInset>
+      <SidebarInset className='overflow-x-auto'>
         <header className="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
