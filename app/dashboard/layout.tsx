d6e8a6ee266d8952cf3055a38f0a289152d71b4e
Kiranism <iamkiran2831@gmail.com>
2024-11-09 23:33:02 +0530

    feat: kbar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import KBar from '@/components/kdb';
+import KBar from '@/components/kbar';
 import AppSidebar from '@/components/layout/app-sidebar';
 import Header from '@/components/layout/header';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
