ebbfbcb08f876c469df2f9c19a34915ea31e6161
Kiranism <iamkiran2831@gmail.com>
2024-11-09 23:56:22 +0530

    feat: kbar styling

--- components/kbar/result-item.tsx
+++ components/kbar/result-item.tsx
@@ -1,6 +1,5 @@
-// ResultItem.tsx
+import type { ActionId, ActionImpl } from 'kbar';
 import * as React from 'react';
-import type { ActionImpl, ActionId } from 'kbar';
 
 const ResultItem = React.forwardRef(
   (
@@ -31,7 +30,7 @@ const ResultItem = React.forwardRef(
         {active && (
           <div
             id="kbar-result-item"
-            className="absolute inset-0 !z-[-1] border-l-4 border-black bg-gray-200 dark:border-white dark:bg-gray-700"
+            className="absolute inset-0 !z-[-1] border-l-4 border-sidebar-primary bg-sidebar-accent"
           ></div>
         )}
         <div className="relative z-10 flex items-center gap-2">
@@ -48,7 +47,7 @@ const ResultItem = React.forwardRef(
               <span>{action.name}</span>
             </div>
             {action.subtitle && (
-              <span className="text-sm text-gray-600 dark:text-gray-400">
+              <span className="text-sm text-muted-foreground">
                 {action.subtitle}
               </span>
             )}
@@ -59,7 +58,7 @@ const ResultItem = React.forwardRef(
             {action.shortcut.map((sc, i) => (
               <kbd
                 key={sc + i}
-                className="flex items-center gap-1 rounded-md border border-gray-200 bg-gray-50 px-1.5 py-1 text-xs font-medium text-gray-600 shadow dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400"
+                className="flex items-center gap-1 rounded-md border px-1.5 py-1 text-xs font-medium  shadow"
               >
                 {sc}
               </kbd>
