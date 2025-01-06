a69d5af44beb3c0403df03736ee95ae9b43e9e88
Kiranism <iamkiran2831@gmail.com>
2025-01-06 23:19:09 +0530

    fix: some fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { auth } from '@/lib/auth';
 import Providers from '@/components/layout/providers';
 import { Toaster } from '@/components/ui/sonner';
 import type { Metadata } from 'next';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { Lato } from 'next/font/google';
 import NextTopLoader from 'nextjs-toploader';
 import './globals.css';
@@ -24,17 +25,15 @@ export default async function RootLayout({
 }) {
   const session = await auth();
   return (
-    <html
-      lang='en'
-      className={`${lato.className}`}
-      suppressHydrationWarning={true}
-    >
+    <html lang='en' className={`${lato.className}`} suppressHydrationWarning>
       <body className={'overflow-hidden'}>
         <NextTopLoader showSpinner={false} />
-        <Providers session={session}>
-          <Toaster />
-          {children}
-        </Providers>
+        <NuqsAdapter>
+          <Providers session={session}>
+            <Toaster />
+            {children}
+          </Providers>
+        </NuqsAdapter>
       </body>
     </html>
   );
