1ad62667b3d55b7be1f77b46f04a0c1775ded164
Andres6936 <andres6936@gmail.com>
2025-01-12 09:08:11 -0500

    Refactor layout components for consistent flex behavior.
    Updated multiple components to use `flex` and `flex-1` for layout consistency and better responsiveness. Adjusted `PageContainer`, tables, and page structure to improve alignment and scrolling functionality. Added `.idea/` to `.gitignore` for IDE-specific files.

--- src/components/ui/table/data-table.tsx
+++ src/components/ui/table/data-table.tsx
@@ -91,58 +91,64 @@ export function DataTable<TData, TValue>({
   });
 
   return (
-    <div className='space-y-4'>
-      <ScrollArea className='grid h-[calc(80vh-220px)] rounded-md border md:h-[calc(90dvh-240px)]'>
-        <Table className='relative'>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
+    <div className='flex flex-1 flex-col space-y-4'>
+      <div className='relative flex flex-1'>
+        <div className='absolute inset-0 flex overflow-scroll rounded-md border'>
+          <ScrollArea className='flex flex-1'>
+            <Table className='relative'>
+              <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    ))}
+                  </TableRow>
                 ))}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && 'selected'}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
+              </TableHeader>
+              <TableBody>
+                {table.getRowModel().rows?.length ? (
+                  table.getRowModel().rows.map((row) => (
+                    <TableRow
+                      key={row.id}
+                      data-state={row.getIsSelected() && 'selected'}
+                    >
+                      {row.getVisibleCells().map((cell) => (
+                        <TableCell key={cell.id}>
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell
+                      colSpan={columns.length}
+                      className='h-24 text-center'
+                    >
+                      No results.
                     </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className='h-24 text-center'
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-        <ScrollBar orientation='horizontal' />
-      </ScrollArea>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
 
-      <div className='flex flex-col items-center justify-end gap-2 space-x-2 py-4 sm:flex-row'>
+            <ScrollBar orientation='vertical' />
+            <ScrollBar orientation='horizontal' />
+          </ScrollArea>
+        </div>
+      </div>
+
+      <div className='flex flex-col items-center justify-end gap-2 space-x-2 py-2 sm:flex-row'>
         <div className='flex w-full items-center justify-between'>
           <div className='flex-1 text-sm text-muted-foreground'>
             {totalItems > 0 ? (
