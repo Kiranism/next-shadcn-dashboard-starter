c5aa30bc8f5fd302b92676a51c84ea4c7b312fdd
Kiranism <iamkiran2831@gmail.com>
2025-01-12 22:47:01 +0530

    chore: na

--- src/features/auth/components/sigin-view.tsx
+++ src/features/auth/components/sigin-view.tsx
@@ -41,11 +41,11 @@ export default function SignInViewPage() {
         <div className='relative z-20 mt-auto'>
           <blockquote className='space-y-2'>
             <p className='text-lg'>
-              &ldquo;This library has saved me countless hours of work and
-              helped me deliver stunning designs to my clients faster than ever
+              &ldquo;This starter template has saved me countless hours of work
+              and helped me deliver projects to my clients faster than ever
               before.&rdquo;
             </p>
-            <footer className='text-sm'>Sofia Davis</footer>
+            <footer className='text-sm'>Random Dude</footer>
           </blockquote>
         </div>
       </div>
