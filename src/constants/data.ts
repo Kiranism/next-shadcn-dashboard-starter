9f23b405b92b19c5f82a4c01e5ed522946a1be9b
Kiranism <iamkiran2831@gmail.com>
2025-01-15 21:26:45 +0530

    feat: overview parallel route

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -59,3 +59,55 @@ export const navItems: NavItem[] = [
     items: [] // No child items
   }
 ];
+
+export interface SaleUser {
+  id: number;
+  name: string;
+  email: string;
+  amount: string;
+  image: string;
+  initials: string;
+}
+
+export const recentSalesData: SaleUser[] = [
+  {
+    id: 1,
+    name: 'Olivia Martin',
+    email: 'olivia.martin@email.com',
+    amount: '+$1,999.00',
+    image: 'https://api.slingacademy.com/public/sample-users/1.png',
+    initials: 'OM'
+  },
+  {
+    id: 2,
+    name: 'Jackson Lee',
+    email: 'jackson.lee@email.com',
+    amount: '+$39.00',
+    image: 'https://api.slingacademy.com/public/sample-users/2.png',
+    initials: 'JL'
+  },
+  {
+    id: 3,
+    name: 'Isabella Nguyen',
+    email: 'isabella.nguyen@email.com',
+    amount: '+$299.00',
+    image: 'https://api.slingacademy.com/public/sample-users/3.png',
+    initials: 'IN'
+  },
+  {
+    id: 4,
+    name: 'William Kim',
+    email: 'will@email.com',
+    amount: '+$99.00',
+    image: 'https://api.slingacademy.com/public/sample-users/4.png',
+    initials: 'WK'
+  },
+  {
+    id: 5,
+    name: 'Sofia Davis',
+    email: 'sofia.davis@email.com',
+    amount: '+$39.00',
+    image: 'https://api.slingacademy.com/public/sample-users/5.png',
+    initials: 'SD'
+  }
+];
