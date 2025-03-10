9aa9d411de9a006188fbe326686b4233ae6065fe
Kiranism <iamkiran2831@gmail.com>
2025-03-10 13:09:21 +0530

    fix: #79 auth js env variable change

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -4,10 +4,7 @@ import GithubProvider from 'next-auth/providers/github';
 
 const authConfig = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID ?? '',
-      clientSecret: process.env.GITHUB_SECRET ?? ''
-    }),
+    GithubProvider({}),
     CredentialProvider({
       credentials: {
         email: {
