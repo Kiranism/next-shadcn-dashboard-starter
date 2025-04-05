7265c015d901199c13b056f4b8e69f240cdd77b9
aminebouichidar <amine.bouichidar@gmail.com>
2025-04-05 05:35:48 +0200

    updates

--- src/app/dashboard/overview/layout.tsx
+++ src/app/dashboard/overview/layout.tsx
@@ -30,7 +30,6 @@ export default function OverViewLayout({
             Hi, Welcome back 👋
           </h2>
         </div>
-
         <div className='*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs md:grid-cols-2 lg:grid-cols-4'>
           <Card className='@container/card'>
             <CardHeader>
