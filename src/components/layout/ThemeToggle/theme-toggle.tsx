9f4276b22073cfa1952543bcd126566e3b884c1a
Kiranism <iamkiran2831@gmail.com>
2025-04-07 11:02:30 +0530

    feat: added theme transition

--- src/components/layout/ThemeToggle/theme-toggle.tsx
+++ src/components/layout/ThemeToggle/theme-toggle.tsx
@@ -9,16 +9,35 @@ import { Button } from '@/components/ui/button';
 export function ModeToggle() {
   const { setTheme, resolvedTheme } = useTheme();
 
-  const toggleTheme = React.useCallback(() => {
-    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
-  }, [resolvedTheme, setTheme]);
+  const handleThemeToggle = React.useCallback(
+    (e?: React.MouseEvent) => {
+      const newMode = resolvedTheme === 'dark' ? 'light' : 'dark';
+      const root = document.documentElement;
+
+      if (!document.startViewTransition) {
+        setTheme(newMode);
+        return;
+      }
+
+      // Set coordinates from the click event
+      if (e) {
+        root.style.setProperty('--x', `${e.clientX}px`);
+        root.style.setProperty('--y', `${e.clientY}px`);
+      }
+
+      document.startViewTransition(() => {
+        setTheme(newMode);
+      });
+    },
+    [resolvedTheme, setTheme]
+  );
 
   return (
     <Button
       variant='secondary'
       size='icon'
       className='group/toggle size-8'
-      onClick={toggleTheme}
+      onClick={handleThemeToggle}
     >
       <IconBrightness />
       <span className='sr-only'>Toggle theme</span>
