d48e00932c8d6f44c7368a6f74abe9f8534ea43f
Kiranism <iamkiran2831@gmail.com>
2025-04-08 19:28:53 +0530

    feat: #95 route protection middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextRequest } from 'next/server';
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
 
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+  if (isProtectedRoute(req)) await auth.protect();
+});
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
