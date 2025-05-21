319ea2786bfeeaef6a89c8cd4fab90e560216db8
Kiranism <iamkiran2831@gmail.com>
2025-05-21 19:15:33 +0530

    feat: sentry inital setup

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,12 +1,33 @@
 import * as Sentry from '@sentry/nextjs';
 
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('../sentry.server.config');
-  }
+const sentryOptions: Sentry.NodeOptions | Sentry.EdgeOptions = {
+  // Sentry DSN
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('../sentry.edge.config');
+  // Enable Spotlight in development
+  spotlight: process.env.NODE_ENV === 'development',
+
+  // Adds request headers and IP for users, for more info visit
+  sendDefaultPii: true,
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false
+};
+
+export async function register() {
+  if (!process.env.NEXT_PUBLIC_SENTRY_DISABLED) {
+    if (process.env.NEXT_RUNTIME === 'nodejs') {
+      // Node.js Sentry configuration
+      Sentry.init(sentryOptions);
+    }
+
+    if (process.env.NEXT_RUNTIME === 'edge') {
+      // Edge Sentry configuration
+      Sentry.init(sentryOptions);
+    }
   }
 }
 
