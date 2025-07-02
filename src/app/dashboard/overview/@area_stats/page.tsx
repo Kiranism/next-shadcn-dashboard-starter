f174191136ce244e1d506b9d8c2b89780d748c3d
darseen <darseen.dev@gmail.com>
2025-07-02 15:55:36 +0300

    use built-in setTimeout from node:timers/promises instead of delay

--- src/app/dashboard/overview/@area_stats/page.tsx
+++ src/app/dashboard/overview/@area_stats/page.tsx
@@ -1,7 +1,7 @@
-import { delay } from '@/constants/mock-api';
+import { setTimeout } from 'node:timers/promises';
 import { AreaGraph } from '@/features/overview/components/area-graph';
 
 export default async function AreaStats() {
-  await await delay(2000);
+  await setTimeout(2000);
   return <AreaGraph />;
 }
--- src/app/dashboard/overview/@sales/page.tsx
+++ src/app/dashboard/overview/@sales/page.tsx
@@ -1,7 +1,7 @@
-import { delay } from '@/constants/mock-api';
+import { setTimeout } from 'node:timers/promises';
 import { RecentSales } from '@/features/overview/components/recent-sales';
 
 export default async function Sales() {
-  await delay(3000);
+  await setTimeout(3000);
   return <RecentSales />;
 }
