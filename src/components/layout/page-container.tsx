2dee6e90cd40733754160d1172509187d1610380
Виктория Малыхина <mixail.sagalaev@bk.ru>
2025-09-22 20:21:15 +0300

    style(layout): left-aligned PageContainer with max-width like other pages

--- src/components/layout/page-container.tsx
+++ src/components/layout/page-container.tsx
@@ -8,5 +8,9 @@ export default function PageContainer({
   scrollable?: boolean;
 }) {
   // scrollable parameter is currently not used but kept for future compatibility
-  return <div className='flex flex-1 p-4 md:px-6'>{children}</div>;
+  return (
+    <div className='flex flex-1 justify-start p-4 md:px-6'>
+      <div className='w-full max-w-[1200px]'>{children}</div>
+    </div>
+  );
 }
