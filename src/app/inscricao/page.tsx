2028221af5c25c08a0b6362cdfd5f1065ed2fcc4
yanlima <yangclima@gmail.com>
2026-06-08 07:52:10 -0300

    feat: add informative section to /inscricao

--- src/app/inscricao/page.tsx
+++ src/app/inscricao/page.tsx
@@ -1,3 +1,4 @@
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { ApplicationForm } from '@/features/selection-process/components/inscription/application-form';
 
 export default function InscricaoPage() {
@@ -10,6 +11,32 @@ export default function InscricaoPage() {
           obrigatórios.
         </p>
       </div>
+      <Alert>
+        <AlertTitle>Atenção</AlertTitle>
+        <AlertDescription className='text-sm space-y-3 mt-1'>
+          <p>Leia atentamente as seguintes informações</p>
+          <p>
+            O nosso Processo Seletivo será realizado através dessa plataforma experimental que ainda
+            está em fase de desenvolvimento e por isso pode apresentar instabilidades, caso encontre
+            algum problema, envie um email para o seguinte endereço:{' '}
+            <a href='mailto:psel@wattconsultoria.com.br' className='underline font-medium'>
+              psel@wattconsultoria.com.br
+            </a>
+          </p>
+          <p>
+            Todas as regras e orientações do nosso Processo Seletivo estão documentadas e podem ser
+            consultadas no seguinte edital:{' '}
+            <a
+              href='https://drive.google.com/file/d/1r4LZt_fyMn7g33hrb_EuaOJb7peCjYg_/view?usp=drive_link'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='underline font-medium'
+            >
+              Edital PSEL 2026.1
+            </a>
+          </p>
+        </AlertDescription>
+      </Alert>
       <ApplicationForm />
     </div>
   );
